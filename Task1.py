1297af1888f8c88f653a21380a2a378d1d59dd89
MilkTeaYao <31473426+MilkTeaYao@users.noreply.github.com>
2019-12-04 04:10:00 -0500

    Add output to JSON and datetime type match.

--- Task1.py
+++ Task1.py
@@ -1,8 +1,11 @@
 import pyspark
 from pyspark import SparkContext
-from pyspark.sql.types import IntegerType, DoubleType, DateType, StringType
+from pyspark.sql import SparkSession
+from pyspark.sql.types import IntegerType, DoubleType, DateType, StringType, LongType, TimestampType, BooleanType
 import re
 from datetime import datetime
+import json
+import dateutil.parser
 
 sc = SparkContext()
 
@@ -13,6 +16,7 @@ spark = SparkSession \
         .getOrCreate()
 
 filePath = '/user/mw4086/NYCOpenData-Sample'
+# filePath = '/user/hm74/NYCOpenData'
 
 fs = spark._jvm.org.apache.hadoop.fs.FileSystem.get(spark._jsc.hadoopConfiguration())
 fileStatusList = fs.listStatus(spark._jvm.org.apache.hadoop.fs.Path(filePath))
@@ -25,67 +29,138 @@ def loadData(fileName):
     data = file.zipWithIndex().filter(lambda tup: tup[1] > 0).map(lambda x: x[0]).map(lambda x: x.split('\t'))
     return header, data, dataFrame
 
-def defineDataType(value):
+def addDataType(dataType, x):
     integer_expr = r'^[+-]?[1-9]\d*$'
-    real_expr = r'^[+-]?(\d+.\d*)|(\d*.\d+)$'
-    if (re.match(integer_expr, value)):
-        return 'INTEGER(LONG)'
-    if (re.match(real_expr, value)):
-        return 'REAL'
-    if (datetime.strptime(value, '%Y-%m-%d') or datetime.strptime(value, '%Y/%m/%d')):
-        return 'DATE/TIME'
-    return 'TEXT'
-
-def processColumnValueWithType(x):
-    if x[0] == 'INTEGER(LONG)':
-        return (x[0], x[1], int(x[1]))
-    elif x[0] == 'INTEGER(LONG)':
-        return (x[0], x[1], float(x[1]))
-    elif x[0] == 'DATE/TIME':
-        return (x[0], x[1], datetime(x[1]))
+    real_expr1 = r'^[+-]?\d+\.\d*$'
+    real_expr2 = r'^[+-]?\d*\.\d+$'
+    if x.strip() == '':
+        return ('TEXT', x, len(x))
+    if dataType == 'INTEGER(LONG)':
+        return (dataType, x, int(x))
+    elif dataType == 'REAL':
+        return (dataType, x, float(x))
+    elif dataType == 'DATE/TIME':
+        return (dataType, x[1], datetime(x[1]))
     else:
-        return (x[0], x[1], len(x[1]))
+        if (re.match(integer_expr, x)):
+            return ('INTEGER(LONG)', x, int(x))
+        elif (re.match(real_expr1, x) or re.match(real_expr2, x)):
+            return ('REAL', x, float(x))
+        else:
+            try:
+                new_x = dateutil.parser.parse(x, default = datetime(1900, 1, 1, 0, 0, 0, 000000))
+                return ('DATE/TIME', x, new_x)
+            except ValueError:
+                pass
+        try:
+            new_x = datetime.strptime(x, '%I%MA')
+            return ('DATE/TIME', x, new_x)
+        except ValueError:
+            pass
+        try:
+            new_x = datetime.strptime(x, '%I%MP')
+            return ('DATE/TIME', x, new_x)
+        except ValueError:
+            pass
+        return ('TEXT', x, len(x))
 
 def processColumn(header, data, dataFrame):
     columnNumber = len(header)
+    columnList = []
     tableKeyList = []
     for i in range(columnNumber):
+        columnDict = {}
         columnName = header[i]
+        columnDict['column_name'] = columnName
         columnValue = data.map(lambda x: x[i])
-        nonEmptyCellNumber = columnValue.filter(lambda x: x != '').count()
-        emptyCellNumber = columnValue.filter(lambda x: x == '').count()
+        nonEmptyCellNumber = columnValue.filter(lambda x: x.strip() != '').count()
+        columnDict['number_non_empty_cells'] = nonEmptyCellNumber
+        emptyCellNumber = columnValue.filter(lambda x: x.strip() == '').count()
+        columnDict['number_empty_cells'] = emptyCellNumber
         valueNumber = columnValue.count()
         distinctValueNumber = columnValue.distinct().count()
+        columnDict['number_distinct_values'] = distinctValueNumber
         if valueNumber == distinctValueNumber:
             tableKeyList.append(columnName)
         top5FrequentValues = columnValue.map(lambda x: (x, 1)).reduceByKey(lambda x1, x2: x1 + x2).takeOrdered(5, key=lambda x: -x[1])
-
+        columnDict['frequent_values'] = list(map(lambda x: x[0], top5FrequentValues))
         dfDataType = dataFrame.schema[i].dataType
         typeMap = {IntegerType: 'INTEGER(LONG)',
+                   LongType: 'INTEGER(LONG)',
                    DoubleType: 'REAL',
-                   DateType: 'DATE/TIME'}
-        if (type(dfDataType) != StringType):
-            dataType = typeMap[type(dfDataType)]
-            typeList = [dataType]
-            columnWithType = columnValue.map(lambda x: (dataType, x))
-        else:
-            columnWithType = columnValue.map(lambda x: (defineDataType(x), x))
-            typeList = columnWithType.keys().distinct().collect()
-        columnProcessValue = columnWithType.map(lambda x: processColumnValueWithType(x))
-        typeCount = columnProcessValue.countByKey()
+                   DateType: 'DATE/TIME',
+                   TimestampType: 'DATE/TIME',
+                   BooleanType: 'TEXT',
+                   StringType: 'TEXT'}
+        dataType = typeMap[type(dfDataType)]
+        columnWithType = columnValue.map(lambda x: addDataType(dataType, x))
+        typeList = columnWithType.keys().distinct().collect()
+        typeCount = columnWithType.countByKey()
+        dataTypeList = []
         for columnType in typeList:
-            typeRow = columnProcessValue.filter(lambda x: x[0] == columnType)
+            dataTypeDict = {}
+            dataTypeDict['type'] = columnType
+            dataTypeDict['count'] = typeCount[columnType]
+            typeRow = columnWithType.filter(lambda x: x[0] == columnType)
             typeValue = typeRow.map(lambda x: x[2])
-            typeInfo = [typeValue.max(), typeValue.min(), typeValue.mean(), typeValue.stdev()]
             if columnType == 'TEXT':
-                top5LongestText = typeRow.takeOrdered(5, key=lambda x: -x[2]).collect()
-                top5ShortestText = typeRow.takeOrdered(5, key=lambda x: x[2]).collect()
+                top5LongestText = typeRow.takeOrdered(5, key=lambda x: -x[2])
+                top5ShortestText = typeRow.takeOrdered(5, key=lambda x: x[2])
                 avgLength = typeValue.sum() / typeValue.count()
+                dataTypeDict['shortest_values'] = list(map(lambda x: x[1], top5ShortestText))
+                dataTypeDict['longest_values'] = list(map(lambda x: x[1], top5LongestText))
+                dataTypeDict['average_length'] = avgLength
+            else:
+                dataTypeDict['max_value'] = typeValue.max()
+                dataTypeDict['min_value'] = typeValue.min()
+                if columnType == 'INTEGER(LONG)' or columnType == 'REAL':
+                    dataTypeDict['mean'] = typeValue.mean()
+                    dataTypeDict['stddev'] = typeValue.stdev()
+            dataTypeList.append(dataTypeDict)
+        columnDict['data_types'] = dataTypeList
+        columnList.append(columnDict)
+    return columnList, tableKeyList
 
+def writeToJson(fileName, dataDict):
+    jsondict = json.dumps(dataDict)
+    f = open(fileName[0:9]+'.json', 'w')
+    f.write(jsondict)
+    f.close()
 
+'''
+datetimef = open('datetimesample.txt', 'w')
+for k, fileName in enumerate(fileNameList):
+    dataFrame = spark.read.csv(filePath + '/' + fileName, header='true', inferSchema='true', sep='\t')
+    schema = dataFrame.schema
+    length = len(schema)
+    for i in range(length):
+        columnType = schema[i].dataType
+        columnName = dataFrame.columns[i]
+        if columnType == DateType or columnType == TimestampType or 'date' in columnName.lower() or 'time' in columnName.lower():
+            dataList = dataFrame.select(columnName).take(5)
+            for d in dataList:
+                datetimef.write('%s\n' % d[columnName])
+            print('-------------------------------------')
+    print('++++++++++++++++++++++++++++++++++++++',k)
+datetimef.close()
+'''
+failFileName = []
 for fileName in fileNameList:
-    header, data, dataFrame = loadData(fileName)
-    processColumn(header, data, dataFrame)
+    try:
+        dataDict = {}
+        dataDict['dataset_name'] = fileName
+        header, data, dataFrame = loadData(fileName)
+        columnList, tableKeyList = processColumn(header, data, dataFrame)
+        dataDict['columns'] = columnList
+        dataDict['key_column_candidates'] = tableKeyList
+        writeToJson(fileName, dataDict)
+        print(fileName)
+    except:
+        failFileName.append(fileName)
+if len(failFileName) > 0:
+    failF = open('failFile.txt', 'w')
+    for fileN in failFileName:
+        failF.write('%s\n' % fileN)
+    failF.close()
 
-
-
+sc.stop()
